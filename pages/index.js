cf8cda9427ce531e8edb0723eaa8658af30e9091
bennychun99 <benny.ng@usp.br>
2021-04-03 03:35:27 -0300

    ajustes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Feed from "react-instagram-authless-feed"
 function Logotipo() {
   return (
     <div>
@@ -21,27 +20,8 @@ function Logotipo() {
 }
 function Menu() {
   return (
-    <div>
-      <div className="inline-block md:hidden">
-        <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="h-12"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-        </button>
-      </div>
 
-      <div className="bg-white p-2 md:flex md:items-center">
+      <div className="bg-white md:p-2 w-screen md:flex items-center">
         <a href="#" className="block md:flex">Nossa história</a>
         <a href="#" className="block md:flex">Nossos projetos</a>
         <a href="#" className="block md:flex">A equipe</a>
@@ -50,13 +30,12 @@ function Menu() {
           Familhagem
         </a>
       </div>
-    </div>
   );
 }
 
 function NavBar() {
   return (
-    <div className="p-2 container h-28 max-w-7xl m-auto bg-red-300 flex justify-between items-center">
+    <div className="md:p-2 container h-28 max-w-7xl m-auto bg-red-300 flex flex-col md:flex-row justify-between items-center">
       <Logotipo />
       <Menu />
     </div>
