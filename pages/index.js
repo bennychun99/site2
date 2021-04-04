1077e47841edff7dc35eed686fc80ce484b174a1
bennychun99 <benny.ng@usp.br>
2021-04-04 01:36:28 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -141,13 +141,15 @@ function Home() {
         >
           <h1 className="text-2xl	font-bold text-center md:m-auto md:max-w-5xl text-5xl text-white">A equipe</h1>
           <p className="text-center md:m-auto md:max-w-5xl text-3xl text-white">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure,
+            <br/> <br/> <br/> <br/> <br/> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure,
             molestias! <br /> Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Cumque, tempora. <br />
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
             voluptate porro minus placeat quis reiciendis debitis nesciunt optio
             harum sequi.
           </p>
+
+          <a href="#" className="bg-green-900 rounded-md">clique aqui</a>
         </section>
       </div>
       <section className="p-2 md:container md:m-auto md:max-w-5xl" id="contato">
